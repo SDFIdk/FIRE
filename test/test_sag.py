1c4ab8d908cf357b6667dcfd789719ef7b8da7c5
AsgerPetersen <asgerpetersen@gmail.com>
2018-11-20 17:10:38 +0100

    pytest skeleton

---
+++ test/test_sag.py
@@ -0,0 +1,21 @@
+import pytest
+from fireapi.model import Sag
+
+
+@pytest.mark.skip(reason="Sag cannot be inseted atm because of missing mapping of sagstype")
+def test_soft_delete(firedb):
+    s0 = Sag(id='xxx', behandler='yyy')
+    firedb.session.add(s0)
+    firedb.session.commit()
+
+    s1 = firedb.session.query(Sag).filter(Sag.id == s0.id).one()
+    assert s1 is s0
+    assert s1.registreringtil is None
+
+    firedb.session.delete(s0)
+    firedb.session.commit()
+
+    s2 = firedb.session.query(Sag).filter(Sag.id == s0.id).one()
+    assert s0 is s1 is s2
+    assert s2.registreringtil is not None
+
