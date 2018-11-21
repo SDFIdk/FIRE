8f5876d5777b4be390b72f69b5f552b95e8efd14
Björn Harrtell <bjorn@wololo.org>
2018-11-21 15:05:37 +0100

    Tweak 3 and cheats

--- misc/oraclelinux/setup.sh
+++ misc/oraclelinux/setup.sh
@@ -3,7 +3,7 @@ yum -y install wget yum-utils bzip2
 mv /etc/yum.repos.d/public-yum-ol7.repo /etc/yum.repos.d/public-yum-ol7.repo.bak  
 wget http://yum.oracle.com/public-yum-ol7.repo -O /etc/yum.repos.d/public-yum-ol7.repo
 yum-config-manager --enable ol7_developer_EPEL ol7_oracle_instantclient
-yum -y install oracle-instantclient18.3-basic.x86_64 oracle-instantclient18.3-sqlplus.x86_64
+yum -y install openssl oracle-instantclient18.3-basic.x86_64 oracle-instantclient18.3-sqlplus.x86_64
 sh -c "echo /usr/lib/oracle/18.3/client64/lib > /etc/ld.so.conf.d/oracle-instantclient.conf"
 ldconfig
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
