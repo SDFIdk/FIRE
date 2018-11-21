a339e00013aa68b7bd5462f2b6423dafbe29aabb
Asger Skovbo Petersen <asgerpetersen@gmail.com>
2018-11-21 15:22:40 +0100

    pytest skeleton (#16)

--- misc/oraclelinux/setup.sh
+++ misc/oraclelinux/setup.sh
@@ -3,12 +3,11 @@ yum -y install wget yum-utils bzip2
 mv /etc/yum.repos.d/public-yum-ol7.repo /etc/yum.repos.d/public-yum-ol7.repo.bak  
 wget http://yum.oracle.com/public-yum-ol7.repo -O /etc/yum.repos.d/public-yum-ol7.repo
 yum-config-manager --enable ol7_developer_EPEL ol7_oracle_instantclient
-yum -y install oracle-instantclient18.3-basic.x86_64 oracle-instantclient18.3-sqlplus.x86_64
+yum -y install openssl oracle-instantclient18.3-basic.x86_64 oracle-instantclient18.3-sqlplus.x86_64
 sh -c "echo /usr/lib/oracle/18.3/client64/lib > /etc/ld.so.conf.d/oracle-instantclient.conf"
 ldconfig
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
 source $HOME/miniconda/bin/activate
-wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
-python -m pip install cx_Oracle pytest --upgrade
+conda env create -f environment.yml
+source $HOME/miniconda/bin/activate fikspunktsregister
