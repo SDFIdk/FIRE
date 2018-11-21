3894df2007014194b6c178ee65d9af42dfd912d0
Björn Harrtell <bjorn@wololo.org>
2018-11-21 11:32:34 +0100

    Add bzip2 and cleanup

--- misc/oraclelinux/setup.sh
+++ misc/oraclelinux/setup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-yum -y install wget
+yum -y install wget yum-utils bzip2
 mv /etc/yum.repos.d/public-yum-ol7.repo /etc/yum.repos.d/public-yum-ol7.repo.bak  
 wget http://yum.oracle.com/public-yum-ol7.repo -O /etc/yum.repos.d/public-yum-ol7.repo
-yum install -y yum-utils
 yum-config-manager --enable ol7_developer_EPEL ol7_oracle_instantclient
 yum -y install oracle-instantclient18.3-basic.x86_64 oracle-instantclient18.3-sqlplus.x86_64
 sh -c "echo /usr/lib/oracle/18.3/client64/lib > /etc/ld.so.conf.d/oracle-instantclient.conf"
