be967315fa8fa8deffa3ee95572d9379a8e0898e
Björn Harrtell <bjornharrtell@users.noreply.github.com>
2018-12-10 13:24:53 +0100

    Import test data as part of CI (#47)

---
+++ test/fixtures/sql/data/FIRE_ADM.PUNKTINFOTYPENAMESPACE.sql
@@ -0,0 +1,34 @@
+﻿SET DEFINE OFF;
+--SQL Statement which produced this data:
+--
+--  SELECT * FROM FIRE_ADM.PUNKTINFOTYPENAMESPACE;
+--
+Insert into PUNKTINFOTYPENAMESPACE
+   (OBJECTID, BESKRIVELSE, NAMESPACE)
+ Values
+   (1, 'Ingen beskrivelse', 'AFM');
+Insert into PUNKTINFOTYPENAMESPACE
+   (OBJECTID, BESKRIVELSE, NAMESPACE)
+ Values
+   (2, 'Ingen beskrivelse', 'ATTR');
+Insert into PUNKTINFOTYPENAMESPACE
+   (OBJECTID, BESKRIVELSE, NAMESPACE)
+ Values
+   (3, 'Ingen beskrivelse', 'IDENT');
+Insert into PUNKTINFOTYPENAMESPACE
+   (OBJECTID, BESKRIVELSE, NAMESPACE)
+ Values
+   (4, 'Ingen beskrivelse', 'NET');
+Insert into PUNKTINFOTYPENAMESPACE
+   (OBJECTID, BESKRIVELSE, NAMESPACE)
+ Values
+   (5, 'Ingen beskrivelse', 'PS');
+Insert into PUNKTINFOTYPENAMESPACE
+   (OBJECTID, BESKRIVELSE, NAMESPACE)
+ Values
+   (6, 'Ingen beskrivelse', 'REGION');
+Insert into PUNKTINFOTYPENAMESPACE
+   (OBJECTID, BESKRIVELSE, NAMESPACE)
+ Values
+   (7, 'Ingen beskrivelse', 'SKITSE');
+COMMIT;
---
+++ test/fixtures/sql/data/readme.txt
@@ -0,0 +1,19 @@
+Der er kun medtaget tabeller som indeholder data
+Data er begrænset til de 100 senest tilkomne punkter
+Data er dannet på baggrund af DDL af 2018-12-03 - se evt. udfordringer med DDL nederst. 
+
+Rækkefølge for import
+FIRE_ADM.PUNKTINFOTYPENAMESPACE.sql
+FIRE_ADM.OBSERVATIONTYPENAMESPACE.sql
+FIRE_ADM.SRIDNAMESPACE.sql
+FIRE_ADM.PUNKTINFOTYPE.sql
+FIRE_ADM.SRIDTYPE.sql
+FIRE_ADM.PUNKT.sql
+FIRE_ADM.PUNKTINFO.sql
+FIRE_ADM.KOORDINAT.sql
+FIRE_ADM.GEOMETRIOBJEKT.sql
+FIRE_ADM.OBSERVATION.sql
+
+Udfordringer med DLL
+Ingen udfordringer
+
