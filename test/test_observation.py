b315f4656383373133ce590b2cc14ace4ac9b0af
Björn Harrtell <bjornharrtell@users.noreply.github.com>
2018-12-20 13:19:03 +0100

    Move tests into respective module and type annotate (#66)

--- test/test_observation.py
+++ test/test_observation.py
@@ -1,9 +1,83 @@
-from fireapi.model import Observation
+import uuid
+import datetime
+from fireapi import FireDb
+from fireapi.model import (
+    Sag,
+    Sagsevent,
+    Sagsinfo,
+    Koordinat,
+    Punkt,
+    Observation,
+    Beregning,
+    Point,
+    Geometry,
+)
 
 
-def test_observation(firedb, observation):
+def test_observation(firedb: FireDb, observation: Observation):
     firedb.session.commit()
-
     o1 = firedb.session.query(Observation).get(observation.objectid)
-
     assert o1.objectid == observation.objectid
+
+
+def test_hent_observationer(firedb: FireDb):
+    os = firedb.hent_observationer((1, 2))
+    assert len(os) is 2
+    os = firedb.hent_observationer((-999, -998))
+    assert len(os) is 0
+
+
+def test_hent_observationer_naer_opstillingspunkt(firedb: FireDb):
+    p = firedb.hent_punkt("7CA9F53D-DE26-59C0-E053-1A041EAC5678")
+    os = firedb.hent_observationer_naer_opstillingspunkt(p, 100)
+    assert len(os) is 2
+    os = firedb.hent_observationer_naer_opstillingspunkt(
+        p, 100, datetime.datetime(2015, 10, 8)
+    )
+    assert len(os) is 2
+    os = firedb.hent_observationer_naer_opstillingspunkt(
+        p, 100, datetime.datetime(2015, 11, 1)
+    )
+    assert len(os) is 1
+    os = firedb.hent_observationer_naer_opstillingspunkt(p, 1000)
+    assert len(os) is 4
+    os = firedb.hent_observationer_naer_opstillingspunkt(
+        p, 1000, datetime.datetime(2015, 10, 8)
+    )
+    assert len(os) is 4
+    os = firedb.hent_observationer_naer_opstillingspunkt(
+        p, 1000, datetime.datetime(2015, 10, 8), datetime.datetime(2015, 10, 9)
+    )
+    assert len(os) is 2
+
+
+def test_hent_observationer_naer_geometri(firedb: FireDb):
+    go = firedb.hent_geometri_objekt("7CA9F53D-DE26-59C0-E053-1A041EAC5678")
+    os = firedb.hent_observationer_naer_geometri(go.geometri, 10000)
+    assert len(os) is 17
+    point = Geometry("POINT (10.4811749340072 56.3061226484564)")
+    os = firedb.hent_observationer_naer_geometri(point, 100)
+    assert len(os) is 2
+    polygon = Geometry(
+        "POLYGON ((10.4811749340072 56.3061226484564, 10.5811749340072 56.3061226484564, 10.5811749340072 56.4061226484564, 10.4811749340072 56.4061226484564, 10.4811749340072 56.3061226484564))"
+    )
+    os = firedb.hent_observationer_naer_geometri(polygon, 100)
+    assert len(os) is 6
+
+
+def test_indset_observation(firedb: FireDb, sag: Sag, punkt: Punkt):
+    observation = Observation(
+        antal=0,
+        observationstypeid="geometrisk_koteforskel",
+        observationstidspunkt=datetime.datetime.utcnow(),
+        opstillingspunkt=punkt,
+        value1=0,
+        value2=0,
+        value3=0,
+        value4=0,
+        value5=0,
+        value6=0,
+        value7=0,
+        value8=0,
+    )
+    firedb.indset_observation(sag, observation)
--- test/test_sag.py
+++ test/test_sag.py
@@ -1,11 +1,12 @@
 import pytest
+from fireapi import FireDb
 from fireapi.model import Sag, Sagsinfo
 
 
 @pytest.mark.skip(
     reason="Sag cannot be inserted atm because of missing mapping of sagstype"
 )
-def test_soft_delete(firedb):
+def test_soft_delete(firedb: FireDb):
     s0 = Sag(id="xxx")
     si0 = Sagsinfo(sag=s0, aktiv="true", behandler="yyy")
     firedb.session.add(si0)
@@ -24,3 +25,19 @@ def test_soft_delete(firedb):
     s2 = firedb.session.query(Sag).filter(Sag.id == s0.id).one()
     assert s0 is s1 is s2
     assert s2.registreringtil is not None
+
+
+def test_hent_sag(firedb: FireDb, sag: Sag):
+    s = firedb.hent_sag(sag.id)
+    assert s.id is sag.id
+
+
+def test_hent_alle_sager(firedb: FireDb):
+    ss = firedb.hent_alle_sager()
+    assert len(ss) > 1
+
+
+def test_indset_sag(firedb: FireDb, guid):
+    sagsinfo = Sagsinfo(aktiv="true", behandler="test")
+    sag = Sag(id=guid, sagsinfos=[sagsinfo])
+    firedb.indset_sag(sag)
