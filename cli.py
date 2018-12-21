89d132ed9f06ea1a032818bfdaf93f6082692fa1
klavs <klavs@septima.dk>
2018-12-21 16:16:45 +0100

    CLI fungerer / test af cli

--- cli.py
+++ cli.py
@@ -1,10 +1,9 @@
 import configparser
-import datetime
-import ast
 
 import os
 
 import click
+from click_datetime import Datetime
 
 from fireapi import FireDb
 from fireapi.model import (
@@ -12,27 +11,28 @@ from fireapi.model import (
 )
 
 from adapter import GamaWriter
-import string
 
 @click.group()
 def cli():
     pass
 
 @cli.command() 
-@click.option("-db", "db_con" , help="Connection-streng til fire database", type=string, required=False)
-@click.option("-o", "--output", type=click.File(mode='w'), help="navn på gama input, der skal skabes (.xml)", default="output.xml")
-@click.option("-g", "--geometri", help="wkt. Anvendes som geometri i udvælgelsen af observationer", required=False, type=string)
+@click.option("-db", "db_con" , help="Connection-streng til fire database. [default: environment variabel %fire-db%]", type=str, required=False)
+@click.option("-o", "--out", "output", type=click.File(mode='w'), help="Navn på gama input, der skal skabes (.xml)", default="output.xml", show_default=True)
+@click.option("-g", "--geometri", help="wkt. Anvendes som geometri i udvælgelsen af observationer", required=False, type=str)
 @click.option("-gf", "--geometrifil", help="Fil, som indeholder en wkt-streng. Anvendes som geometri i udvælgelsen af observationer", required=False, type=click.File(mode='r'))
-@click.option("-b", "--buffer", help="Den buffer omkring den givne geometri som skal bruges i udvælgelsen af observationer", required=False, type=int, default=0)
-@click.option("-d", "--fratil", nargs=2, help="Fra- og tildato, som bruges i udvælgelsen af observationer", required=False, type=datetime)
-@click.option("-f", "--fixpunkter", "fixpunkter", help="Komma-separeret liste af punkt-id'er, som skal fastholdes", required=False, type=string)
+@click.option("-b", "--buffer", help="Den buffer omkring den givne geometri som skal bruges i udvælgelsen af observationer", required=False, type=int, default=0, show_default=True)
+@click.option("-df", "--fra", help="Fra-dato, som bruges i udvælgelsen af observationer", required=False, type=Datetime(format='%d-%m-%Y'))
+@click.option("-dt", "--til", help="Til-dato, som bruges i udvælgelsen af observationer", required=False, type=Datetime(format='%d-%m-%Y'))
+@click.option("-f", "--fixpunkter", "fixpunkter", help="Komma-separeret liste af punkt-id'er, som skal fastholdes", required=False, type=str)
 @click.option("-ff", "--fixpunkterfil", "fixpunkterfil", help="Fil, som indeholder komma-separeret liste af punkt-id'er, som skal fastholdes", required=False, type=click.File(mode='r'))
+@click.option("-pf", "--parameterfil", help="Fil, som indeholder netværks-parametre og -attributter", required=False, default="fire-gama.ini", show_default=True, type=click.File(mode='r'))
 
-def write(db, output, geometri, geometrifil, buffer, fratil, fixpunkter, fixpunkterfil):
+def write(db_con, output, geometri, geometrifil, buffer, fra, til, fixpunkter, fixpunkterfil, parameterfil):
     """Create a gama input file"""
-    if db is None:
-        db = os.environ.get("fire_db")
-    fireDb = FireDb(db)
+    if db_con is None:
+        db_con = os.environ.get("fire-db")
+    fireDb = FireDb(db_con)
     writer = GamaWriter(fireDb, output)
     
     g = None
@@ -46,13 +46,11 @@ def write(db, output, geometri, geometrifil, buffer, fratil, fixpunkter, fixpunk
             wkt = geometrifil.read()
             g = Geometry(wkt)
             
-        if fratil is not None:
-            os = fireDb.hent_observationer_naer_geometri(g, buffer, fratil[0], fratil[1])
+        if fra is not None and til is not None:
+            observations = fireDb.hent_observationer_naer_geometri(g, buffer, fra, til)
         else:
-            os = fireDb.hent_observationer_naer_geometri(g, buffer)
-
-        
-        writer.take_observations(os)
+            observations = fireDb.hent_observationer_naer_geometri(g, buffer)
+        writer.take_observations(observations)
     
     if fixpunkter is not None or fixpunkterfil is not None:
         if fixpunkter is not None:
@@ -60,12 +58,13 @@ def write(db, output, geometri, geometrifil, buffer, fratil, fixpunkter, fixpunk
         else:
             fixpunkter_literal = fixpunkterfil.read()
             
-        fixpunkter_list= ast.literal_eval("[" + fixpunkter_literal +"]")
+        fixpunkter_list = fixpunkter_literal.split(",")
+        #fixpunkter_list= ast.literal_eval("[" + fixpunkter_literal +"]")
         writer.set_fixed_point_ids(fixpunkter_list)
     
     parameters = configparser.ConfigParser()
-    parameters.read('fire-gama.ini')
-    writer.write(True, False, "Created by fire-gama", None)
+    parameters.read(parameterfil)
+    writer.write(True, False, "Created by fire-gama", parameters)
 
 
 @cli.command()
--- test/write/cli_test.py
+++ test/write/cli_test.py
@@ -2,8 +2,19 @@ import click
 from click.testing import CliRunner
 from cli import cli
 
+def do_test(runner, args):
+    result = runner.invoke(cli, args)
+    if result.exit_code != 0:
+        click.echo("Failed: " + str(args))
+        click.echo("Exception: " + result.output)
+        return False, result.exception
+    else:
+        click.echo("Success: " + str(args))
+        return True, result.output
+
 if __name__ == "__main__":
     runner = CliRunner()
-    result = runner.invoke(cli, ['write', '-o output/cli_output.xml', '-g POINT (10.4811749340072 56.3061226484564)', '-b 10000', '-f 7CA9F53D-DAE9-59C0-E053-1A041EAC5678'])
-    a = 2
-    
+    args = ['write', '-o', 'output/cli_output_near_geometry.xml', '-g', 'POINT (10.4811749340072 56.3061226484564)', '-b', '10000', '-f', '7CA9F53D-DAE9-59C0-E053-1A041EAC5678']
+    success, return_text = do_test(runner, args)
+    args = ['write', '-o', 'output/cli_output_near_geometry_fra_til.xml', '-g', 'POINT (10.4811749340072 56.3061226484564)', '-b', '10000', '-f', '7CA9F53D-DAE9-59C0-E053-1A041EAC5678', '-df', '08-10-2015', '-dt', '09-10-2015']
+    success, return_text = do_test(runner, args)
