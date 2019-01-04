89ab2498d233920a37e1395a53ad063b7c11d901
AsgerPetersen <asgerpetersen@gmail.com>
2019-01-04 12:36:33 +0100

    Close existing version of koordinat with same srid

--- fireapi/firedb.py
+++ fireapi/firedb.py
@@ -192,6 +192,76 @@ class FireDb(object):
         self.session.add(sagsevent)
         beregning.sagsevent = sagsevent
         for koordinat in beregning.koordinater:
+            if not self._is_new_object(koordinat):
+                raise Exception(
+                    f"Added beregning cannot refer to existing koordinat: {koordinat}"
+                )
+            self._close_existing_koordinat(koordinat)
             koordinat.sagsevent = sagsevent
         self.session.add(beregning)
         self.session.commit()
+
+    # Private methods
+    def _is_new_object(self, obj):
+        """Check that the object has not been persisted to the database (= is 'new').
+
+        Parameters
+        ----------
+        obj: object
+            Object to check.
+
+        Returns
+        -------
+        bool
+            True if object has not been persisted. False otherwise
+        """
+        # here are the five states:
+        # state.transient   # !session & !identity_key
+        # state.pending     # session & !identity_key
+        # state.persistent  # session &  identity_key
+        # state.detached    # !session &  identity_key
+        # state.deleted     # session & identity_key, flushed but not committed. Commit moves it to detached state
+        insp = inspect(obj)
+        return not (insp.persistent or insp.detached)
+
+    def _close_existing_koordinat(self, newkoordinat):
+        """Checks if a previous version of the new koordinat exists. If it exists the existing koordinat is closed.
+
+        Parameters
+        ----------
+        newkoordinat: koordinat
+            New koordinat object to insert into database. The koordinat must have its punkt-reference set before calling
+            this method.
+        """
+        if not self._is_new_object(newkoordinat):
+            return
+
+        if not newkoordinat.punkt:
+            raise Exception("koordinat must have a reference to its punkt")
+
+        # Find existing koordinat with the same properties
+        # According to constraint "KOOR_UNIQ_001" koordinates must be unique with regard to
+        # SRID, PUNKTID, REGISTRERINGTIL
+        # Note that new koordinat MAY have int srid
+        existing_koordinater = list(
+            [
+                k
+                for k in newkoordinat.punkt.koordinater
+                if str(k.srid) == str(newkoordinat.srid)
+                and k.registreringtil is None
+                and k is not newkoordinat
+            ]
+        )
+        assert (
+            len(existing_koordinater) <= 1
+        ), f"Punkt has more than one koordinat with srid={newkoordinat.srid} and registeringtil=None"
+
+        if existing_koordinater:
+            # self.session.expunge(newkoordinat)
+            # Close the existing version (if any)
+            for k in existing_koordinater:
+                k._registreringtil = func.sysdate()
+            # self.session.add(newkoordinat)
+
+
+#
--- test/test_beregning.py
+++ test/test_beregning.py
@@ -24,3 +24,38 @@ def test_indset_beregning(firedb: FireDb, sag: Sag, punkt: Punkt):
     koordinat = Koordinat(srid=-1, transformeret="false", punkt=punkt)
     beregning.koordinater.append(koordinat)
     firedb.indset_beregning(sag, beregning)
+
+
+def test_indset_beregning_invalidates_existing_koordinat(
+    firedb: FireDb, sag: Sag, punkt: Punkt
+):
+    observation = Observation(
+        antal=0,
+        observationstypeid="geometrisk_koteforskel",
+        observationstidspunkt=datetime.datetime.utcnow(),
+        opstillingspunkt=punkt,
+        value1=0,
+        value2=0,
+        value3=0,
+        value4=0,
+        value5=0,
+        value6=0,
+        value7=0,
+        value8=0,
+    )
+    firedb.indset_observation(sag, observation)
+    beregning = Beregning()
+    beregning.observationer.append(observation)
+    koordinat = Koordinat(srid="-1", transformeret="false", punkt=punkt)
+    beregning.koordinater.append(koordinat)
+    firedb.indset_beregning(sag, beregning)
+
+    # new beregning of the same observation with a new koordinat
+    beregning2 = Beregning()
+    beregning2.observationer.append(observation)
+    koordinat2 = Koordinat(srid="-1", transformeret="false", punkt=punkt)
+    beregning2.koordinater.append(koordinat2)
+    firedb.indset_beregning(sag, beregning2)
+
+    assert len(punkt.koordinater) == 2
+    assert len([k for k in punkt.koordinater if k.registreringtil is None]) == 1
