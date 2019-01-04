b379895bc52947652a9443ceb696cf3e99514194
AsgerPetersen <asgerpetersen@gmail.com>
2019-01-04 14:57:25 +0100

    Add missing sagstyper

--- fireapi/model/sagstyper.py
+++ fireapi/model/sagstyper.py
@@ -1,10 +1,23 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
+import enum
+from sqlalchemy import Column, String, Integer, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 
-from fireapi.model import RegisteringTidObjekt
-from fireapi.model import RegisteringFraObjekt
+from fireapi.model import RegisteringTidObjekt, RegisteringFraObjekt, DeclarativeBase
 
-# TODO: Sag and Sagsevent are supposed to get remodeled into Sag, SagInfo, Sagevent and SageventInfo
+
+# Model this as a hard coded enum for now. This makes it a lot easier for the user. It would be nice to sync this with
+# the db table eventtype
+class EventType(enum.Enum):
+    KOORDINAT_BEREGNET = "koordinat_beregnet"
+    KOORDINAT_NEDLAGT = "koordinat_nedlagt"
+    OBSERVATION_INDSAT = "observation_indsat"
+    OBSERVATION_NEDLAGT = "observation_nedlagt"
+    PUNKTINFO_TILFOEJET = "punktinfo_tilføjet"
+    PUNKTINFO_FJERNET = "punktinfo_fjernet"
+    PUNKT_OPRETTET = "punkt_oprettet"
+    PUNKT_NEDLAGT = "punkt_nedlagt"
+    BEREGNING = "beregning"
+    KOMMENTAR = "kommentar"
 
 
 class Sag(RegisteringFraObjekt):
@@ -20,7 +33,6 @@ class Sag(RegisteringFraObjekt):
 
 class Sagsinfo(RegisteringTidObjekt):
     __tablename__ = "sagsinfo"
-    objectid = Column(Integer, primary_key=True)
     aktiv = Column(String(5), nullable=False)
     journalnummer = Column(String)
     behandler = Column(String, nullable=False)
@@ -32,11 +44,13 @@ class Sagsinfo(RegisteringTidObjekt):
 class Sagsevent(RegisteringFraObjekt):
     __tablename__ = "sagsevent"
     id = Column(String(36), nullable=False)
-    event = Column(String(4000), nullable=False)
+    event = Column(Enum(EventType), nullable=False)
     # beskrivelse = Column(String)
     sagid = Column(String(36), ForeignKey("sag.id"), nullable=False)
     sag = relationship("Sag", back_populates="sagsevents")
-    # TODO: Eventtype, materiale, and rapporthtml
+    sagseventinfos = relationship(
+        "SagseventInfo", order_by="SagseventInfo.objectid", back_populates="sagsevent"
+    )
     # Fikspunktregisterobjekter
     punkter = relationship(
         "Punkt", order_by="Punkt.objectid", back_populates="sagsevent"
@@ -58,3 +72,41 @@ class Sagsevent(RegisteringFraObjekt):
     beregninger = relationship(
         "Beregning", order_by="Beregning.objectid", back_populates="sagsevent"
     )
+
+
+class SagseventInfo(RegisteringTidObjekt):
+    __tablename__ = "sagseventinfo"
+    beskrivelse = Column(String(4000))
+    sagseventid = Column(String(36), ForeignKey("sagsevent.id"), nullable=False)
+    sagsevent = relationship("Sagsevent", back_populates="sagseventinfos")
+    materialer = relationship(
+        "SagseventInfoMateriale",
+        order_by="SagseventInfoMateriale.objectid",
+        back_populates="sagseventinfo",
+    )
+    rapporthtmler = relationship(
+        "SagseventInfoRapportHtml",
+        order_by="SagseventInfoRapportHtml.objectid",
+        back_populates="sagseventinfo",
+    )
+
+
+class SagseventInfoMateriale(DeclarativeBase):
+    __tablename__ = "sagseventinfo_materiale"
+    objectid = Column(Integer, primary_key=True)
+    md5sum = Column(String(32), nullable=False)
+    sti = Column(String(4000), nullable=False)
+    sagseventinfoobjectid = Column(
+        Integer, ForeignKey("sagseventinfo.objectid"), nullable=False
+    )
+    sagseventinfo = relationship("SagseventInfo", back_populates="materialer")
+
+
+class SagseventInfoRapportHtml(DeclarativeBase):
+    __tablename__ = "sagseventinfo_rapporthtml"
+    objectid = Column(Integer, primary_key=True)
+    rapporthtml = Column(String, nullable=False)
+    sagseventinfoobjectid = Column(
+        Integer, ForeignKey("sagseventinfo.objectid"), nullable=False
+    )
+    sagseventinfo = relationship("SagseventInfo", back_populates="rapporthtmler")
