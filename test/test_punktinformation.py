7354b6a4e353f42a3474630ccb8d75b72eef6f63
AsgerPetersen <asgerpetersen@gmail.com>
2019-01-07 14:34:19 +0100

    Refactor PunktInformation.infotype

--- test/test_punktinformation.py
+++ test/test_punktinformation.py
@@ -2,5 +2,5 @@ from fireapi.model import *
 
 
 def test_indset_punktinformation(firedb, sag, punkt, punktinformationtype):
-    pi = PunktInformation(infotype=punktinformationtype.infotype, punkt=punkt)
+    pi = PunktInformation(infotype=punktinformationtype, punkt=punkt)
     firedb.indset_punktinformation(Sagsevent(sag=sag), pi)
