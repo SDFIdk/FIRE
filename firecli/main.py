3d830c90b3f806e5f661bb9b852e022ac2982bf3
Kristian Evers <kristianevers@gmail.com>
2019-01-23 16:12:15 +0100

    Hjælptekst på dansk, samt --version option

--- firecli/main.py
+++ firecli/main.py
@@ -6,9 +6,12 @@ from pkg_resources import iter_entry_points
 import click
 from click_plugins import with_plugins
 
+import firecli
+
 @with_plugins(iter_entry_points('firecli.fire_commands'))
 @click.group()
 @click.help_option(help='Vis denne hjælp tekst')
+@click.version_option(version=firecli.__version__, prog_name='fire')
 def fire():
     '''
     🔥 Kommandolinje adgang til FIRE.
