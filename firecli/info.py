03986c9742a9fd0437e5cfe04d1ac8c9f711beda
Thomas Knudsen <busstoptaktik@users.noreply.github.com>
2019-03-14 15:03:18 +0100

    Forbedret koordinatformat (#2)
    *  Sorter koordinater alfabetisk efter srid.name
    *  flyt dato frem i front af linjen og trunker efter dag
    *  Giv srid.name (tekst, fx EPSG:25832), frem for sridid (tal, fx 342)
    
    Giver alt i alt lidt bedre overblik (IMHO)

--- firecli/info.py
+++ firecli/info.py
@@ -1,3 +1,4 @@
+import datetime
 import sys
 import itertools
 
@@ -62,8 +63,9 @@ def punkt(ident: str, **kwargs) -> None:
     firecli.print("")
 
     firecli.print("--- KOORDINATER ---", bold=True)
+    punkt.koordinater.sort(key=lambda x: x.srid.name, reverse=False)
     for koord in punkt.koordinater:
-        line = f"  {koord.sridid:20}:  {koord.x}, {koord.y}, {koord.z}, {koord.t}"
+        line = f"    {koord.t.strftime('%Y-%m-%d')}   {koord.srid.name:<15.15} {koord.x}, {koord.y}, {koord.z}"
         if koord.registreringtil is not None:
             firecli.print(line, fg="red")
         else:
