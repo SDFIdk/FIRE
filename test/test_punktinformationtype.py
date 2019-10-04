1c776c8c592ffb19d24aaf1360b0962cb86ddc46
Kristian Evers <kristianevers@gmail.com>
2019-10-04 15:52:11 +0200

    Add indset_*type functions
    indset_punktinformationtype
    indset_observationtype
    indset_srid

--- test/test_punktinformationtype.py
+++ test/test_punktinformationtype.py
@@ -16,3 +16,15 @@ def test_hent_punktinformationtyper_for_namespace(firedb):
     all = list(firedb.hent_punktinformationtyper())
     filter = list(firedb.hent_punktinformationtyper(namespace="AFM"))
     assert len(all) > len(filter)
+
+
+def test_indset_punktinformationtype(firedb):
+    infotype = PunktInformationType(
+        name="ATTR:TEST",
+        anvendelse=PunktInformationTypeAnvendelse.FLAG,
+        beskrivelse="Bare en test",
+    )
+    firedb.indset_punktinformationtype(infotype)
+    typ = firedb.hent_punktinformationtype("ATTR:TEST")
+
+    assert typ.beskrivelse == infotype.beskrivelse
