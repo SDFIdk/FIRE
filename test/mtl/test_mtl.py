d11d04d6a497d94fefb67d0aaf87ba39466da34a
Thomas Knudsen <busstoptaktik@users.noreply.github.com>
2020-05-13 11:02:24 +0200

    Mtl test (#129)
    * Forberedelser til mere omfattende tests af mtl
    
    * Lad mtl-test arbejde fra eget bibliotek

---
+++ test/mtl/test_mtl.py
@@ -0,0 +1,26 @@
+import os
+
+import click
+import pytest
+
+from click.testing import CliRunner
+from fire.cli.mtl import mtl
+
+
+def test_cli():
+    runner = CliRunner()
+
+    title = "go"
+    args = ["go", "bananas"]
+
+    click.echo("\nTest: " + title)
+    click.echo(" Emulating: fire mtl " + " ".join(args))
+
+    cwd = os.getcwd()
+    # As os.chdir(os.path.dirname(__file__)), but using the absolute path
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    result = runner.invoke(mtl, args)
+    os.remove("resultat.xlsx")
+    os.chdir(cwd)
+
+    assert result.exit_code == 0
