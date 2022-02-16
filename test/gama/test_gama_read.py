e57393f330270f186ac5f40593512c0a80a193c3
Kristian Evers <kristianevers@gmail.com>
2022-02-16 19:46:59 +0100

    Undgå warnings i gama tests

--- test/gama/test_gama_read.py
+++ test/gama/test_gama_read.py
@@ -6,7 +6,7 @@ from fire.api.gama import GamaReader
 
 
 def test_read(firedb: FireDb, sag: Sag):
-    input_stream = open(Path(__file__).resolve().parent / "input/all_points.xml", "r")
-    reader = GamaReader(firedb, input_stream)
-
-    reader.read(sag.id)
+    filename = Path(__file__).resolve().parent / "input/all_points.xml"
+    with open(filename, "r") as input_stream:
+        reader = GamaReader(firedb, input_stream)
+        reader.read(sag.id)
