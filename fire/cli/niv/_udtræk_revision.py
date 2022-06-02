bdfc05fa0ef0045ffb25f3a5725bc11c99e54f3d
Joachim Mortensen <xidus@users.noreply.github.com>
2022-06-02 13:01:17 +0200

    Bugfix: Fjern antagelse om, at punkt_informationer har elementer.
    Introduceret for at gøre CI-test glad (med test-data forskellige fra test-databasen), men sikrer os dermed mod denne edge-case.
    
    Refaktorisér prioriterede attributter og lad prioriteringen være optional, men eksplicit.
    
    Tilføj funktionalitet jævnfør TODO og underbyg med test-funktionalitet.
    
    Forsimpl sortering (og formindsk kompleksitet) af prioriterede attributter. Nu giver funktionen det forventede output (ingen omrokering af de 'urørte' atributter).

--- fire/cli/niv/_udtræk_revision.py
+++ fire/cli/niv/_udtræk_revision.py
@@ -1,10 +1,15 @@
 from typing import Final
+from functools import partial
 
 import click
 from sqlalchemy.sql import text
+from fire.api.model.punkttyper import PunktInformationType
 
 from fire.ident import klargør_identer_til_søgning
-from fire.api.model import Punkt
+from fire.api.model import (
+    Punkt,
+    PunktInformation,
+)
 from fire.api.model.geometry import (
     normaliser_lokationskoordinat,
 )
@@ -59,6 +64,15 @@ Disse attributter indgår ikke i punktrevisionen
 (men det diskvalificerer ikke et punkt at have dem)
 """
 
+ATTRIBUT_PRIORITERING: Final[list[str]] = [
+    "ATTR:muligt_datumstabil",
+    "ATTR:beskrivelse",
+]
+"""
+Attributter, der, hvis de eksisterer for et punkt, skal
+være øverst og i angivne rækefølge under hvert punkt.
+"""
+
 LOKATION_DEFAULT: Final[tuple[float, float]] = (11.0, 56.0)
 
 
@@ -95,9 +109,9 @@ def hent_punkter_i_opmålingsdistrikter(
     return fire.cli.firedb.session.query(Punkt).from_statement(stmt).all()
 
 
-def lokations_streng(lokation: tuple[float, float]) -> str:
+def lokationskoordinat_streng(lokation: tuple[float, float]) -> str:
     """
-    Returnerer teksstreng-repræsentation af lokationskoordinat.
+    Returnerer tekst-repræsentation af lokationskoordinat.
 
     Centimeterafrunding for lokationskoordinaten er rigeligt.
 
@@ -105,77 +119,67 @@ def lokations_streng(lokation: tuple[float, float]) -> str:
     return f"{lokation[1]:.3f} m   {lokation[0]:.3f} m"
 
 
+
+def punkt_informationer_aktive(punkt_informationer: list[PunktInformation]) -> list[PunktInformation]:
+    return [
+        info
+        for info in punkt_informationer
+        if info.registreringtil is None
+    ]
+
+
+def fjern_ignorerede(punkt_informationer: list[PunktInformation], ignorerede: list[str]) -> list[PunktInformation]:
+    return [
+        info
+        for info in punkt_informationer
+        if info.infotype.name not in ignorerede
+    ]
+
+
+def har_landsnr_lig_ident(info: PunktInformation, ident: str) -> bool:
+    """Vis kun landsnr for punkter med GM/GI/GNSS-primærident"""
+    return info.infotype.name == "IDENT:landsnr" and info.tekst == ident
+
+
+def fjern_alle_med_ident(punkt_informationer: list[PunktInformation], ident: str) -> list[PunktInformation]:
+    return [
+        info
+        for info in punkt_informationer
+        if not har_landsnr_lig_ident(info, ident)
+    ]
+
+
 def flyt_attributter_til_toppen(
-    punkt_informationer: list, prioritering: list[str]
+    punkt_informationer: list[PunktInformation], *, prioritering: list[str] = ATTRIBUT_PRIORITERING
 ) -> list:
     """
     Find prioriterede attributter og placér dem øverst i samme rækkefølge.
 
-    Funktionen finder først den nye rækkefølge for punktinformationer
-    (linjer med attribut-data), hvorefter den foretager ombytningen af
-    de enkelte linjer efter deres indekser.
-
-    Forsimplet sker følgende:
-
-        Input:
-            punkt_informationer = [c, b, a, x, y, z]
-            prioritering = [z, y, x]
-
-        Output:
-            punkt_informationer = [z, y, x, a, b, c]
-
-        Foretagede ombytninger:
-
-            [0 <-> 5] <=> [c <-> z]
-            [1 <-> 4] <=> [b <-> y]
-            [2 <-> 3] <=> [a <-> x]
-
-        BEMÆRK: Har de prioriterede attributter en anden rækkefølge
-                end i punktinformationerne, bliver de flyttede
-                attributter tilsvarende ombyttet i deres rækkefølge.
+    Resten af atributterne får samme sorterings-indeks, hvorfor
+    deres rækkefølge i forhold til hinanden forbliver uændret.
 
     """
-    # Dét næste ledige linje-indeks, som den næste prioriterede attribut flyttes til
-    index_offset = 0
+    def key(info: PunktInformation) -> int:
+        if info.infotype.name not in prioritering:
+            return 9999
+        return prioritering.index(info.infotype.name)
 
-    # Liste med indekser for de eksisterende punktinformationslinjer
-    indices = list(range(len(punkt_informationer)))
+    return sorted(punkt_informationer, key=key)
 
-    # Flyt attributter til toppen i samme rækkefølge som angivet i prioritering
-    # Første attribut står på første linje for punktet
-    # Anden attribut står på anden linje for punktet
-    # og så videre.
-    for attribut_navn in prioritering:
 
-        # Antag, at attributten ikke findes, og indeks dermed ikke skal
-        # stige, inden vi går til den næste prioriterede attribut.
-        skift_indeks = False
+def har_infotype(punkt_information: PunktInformation, attribut: str) -> bool:
+    return punkt_information.infotype.name == attribut
 
-        # Start forfra med at løbe over linjerne for punktet
-        for index, info in enumerate(punkt_informationer):
 
-            # Fortsæt, hvis linjen ikke indeholder den aktuelle attribut
-            if info.infotype.name != attribut_navn:
-                continue
+mulig_datumstabil = partial(har_infotype, attribut="ATTR:muligt_datumstabil")
 
-            # Byt plads med den punktinformation placeret ved `index_offset`
-            # Analogi: a, b = b, a
-            indices[index_offset], indices[index] = (
-                indices[index],
-                indices[index_offset],
-            )
 
-            # Da attributten blev fundet, er det nødvendigt at flytte
-            # indeks for ombytningslinjen til den efterfølgende:
-            skift_indeks = True
-            break
-
-        # Skift til næste linje-indeks, hvis aktuelle attribut
-        # blev fundet og sat på det aktuelle indeks, ellers ikke.
-        index_offset += 1 if skift_indeks else 0
-
-    # Sortér efter rækkefølgen i `indices`
-    return [punkt_informationer[i] for i in indices]
+def har_attr_muligt_datumstabil(punkt_informationer: list[PunktInformation]) -> bool:
+    return any(
+        mulig_datumstabil(punkt_info)
+        for punkt_info
+        in punkt_informationer
+    )
 
 
 @niv_command_group.command()
@@ -242,12 +246,11 @@ def udtræk_revision(
             lokation = LOKATION_DEFAULT
 
         lokation = normaliser_lokationskoordinat(lokation[0], lokation[1], "DK", True)
-        lokation_repr = lokations_streng(lokation)
+        lokation_repr = lokationskoordinat_streng(lokation)
         revision = revision.append(
             {
                 "Punkt": ident,
                 "Attribut": "LOKATION",
-                # Centimeterafrunding for lokationskoordinaten er rigeligt
                 "Tekstværdi": lokation_repr,
                 "Ny værdi": lokation_repr,
                 "id": lokations_id,
@@ -257,31 +260,17 @@ def udtræk_revision(
         )
 
         # Fjern punktinformationer, der ikke skal skrives til arket
-        def har_landsnr_lig_ident(info) -> bool:
-            """Vis kun landsnr for punkter med GM/GI/GNSS-primærident"""
-            return info.infotype.name == "IDENT:landsnr" and info.tekst == ident
+        punkt_informationer = punkt_informationer_aktive(punkt.punktinformationer)
+        punkt_informationer = fjern_ignorerede(punkt_informationer, ignorerede_attributter)
+        punkt_informationer = fjern_alle_med_ident(punkt_informationer, ident)
 
-        punkt_informationer: list = [
-            info
-            for info in punkt.punktinformationer
-            if info.registreringtil is None
-            and info.infotype.name not in ignorerede_attributter
-            and not har_landsnr_lig_ident(info)
-        ]
+        # Inden punkt-informationerne føjes til regnearket, flyt
+        # prioriterede attributter til toppen i den valgte rækkefølge.
+        punkt_informationer = flyt_attributter_til_toppen(punkt_informationer)
 
-        # Flyt attributter til toppen af listen med punktinformationer,
-        # så de kommer først og i denne rækkefølge under hvert punkt.
-        prioritering = [
-            "ATTR:muligt_datumstabil",
-            "ATTR:beskrivelse",
-        ]
-        punkt_informationer = flyt_attributter_til_toppen(
-            punkt_informationer, prioritering
-        )
-
-        # Tilføj anden række til arket, hvis denne attribut ikke var sat i forvejen
-        # Rationale: Spar måleren for indtastning, hvis de ønsker at tilføje denne attribut.
-        if not punkt_informationer[0].infotype.name == "ATTR:muligt_datumstabil":
+        # Spar måleren for indtastning, hvis de ønsker at tilføje denne attribut:
+        # Tilføj række til arket, hvis attribut om mulig datumstabil ej sat i forvejen
+        if not har_attr_muligt_datumstabil(punkt_informationer):
             attribut = {
                 "Attribut": "ATTR:muligt_datumstabil",
                 "Sluk": "x",
