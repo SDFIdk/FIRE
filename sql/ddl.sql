a8161096821b91d205a0ec79e92bc0414ec43c50
Stefan Krebs <stkre@sdfi.dk>
2024-07-24 10:00:31 +0200

    Ret Before Update triggers til After Update
    Gøres på tabellerne Sag, Sagsinfo og Sagseventinfo. Gøres for at
    skabe konsistens med alle de andre 9 triggers som udfører samme
    tjek på andre tabeller.
    
    Triggerne tjekker alle at man ikke kan overskrive bestemte felter
    relateret til Sagshistorik, herunder: objektid, registrering- og
    sagseventfra, punktid, sagsid, etc.

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1432,8 +1432,8 @@ END;
 /
 
 
-CREATE OR REPLACE TRIGGER sag_bu_trg
-BEFORE UPDATE ON sag
+CREATE OR REPLACE TRIGGER sag_au_trg
+AFTER UPDATE ON sag
 FOR EACH ROW
 BEGIN
   IF :new.objektid != :old.objektid THEN
@@ -1476,8 +1476,8 @@ end;
 
 
 
-CREATE OR REPLACE TRIGGER sagseventinfo_bu_trg
-BEFORE UPDATE ON sagseventinfo
+CREATE OR REPLACE TRIGGER sagseventinfo_au_trg
+AFTER UPDATE ON sagseventinfo
 FOR EACH ROW
 BEGIN
   IF :new.objektid != :old.objektid THEN
@@ -1499,8 +1499,8 @@ END;
 /
 
 
-CREATE OR REPLACE TRIGGER sagsinfo_bu_trg
-BEFORE UPDATE ON sagsinfo
+CREATE OR REPLACE TRIGGER sagsinfo_au_trg
+AFTER UPDATE ON sagsinfo
 FOR EACH ROW
 BEGIN
   IF :new.objektid != :old.objektid THEN
