d2b7dfc1bf6319c42f821a281f267b280e987240
Stefan Krebs <stkre@sdfi.dk>
2024-08-19 15:30:07 +0200

    Fjern kolonnen referenceramme på Tidsserie
    Denne kolonne indeholdt ikke mere information end hvad man kan
    finde i sridtype-tabellen, og den nyligt oprettede kolonne
    kortnavn.

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -127,7 +127,6 @@ CREATE TABLE tidsserie (
 
   navn VARCHAR2(4000) NOT NULL UNIQUE,
   formaal VARCHAR2(4000) NOT NULL,
-  referenceramme VARCHAR2(50) NOT NULL,
   sridid INTEGER NOT NULL,
   tstype INTEGER NOT NULL
 );
@@ -856,7 +855,6 @@ COMMENT ON COLUMN tidsserie.sagseventfraid IS 'Angivelse af den hændelse der ha
 COMMENT ON COLUMN tidsserie.sagseventtilid IS 'Angivelse af den hændelse der har bevirket afregistrering af et fikspunktsobjekt';
 COMMENT ON COLUMN tidsserie.navn IS 'Tidsseriens navn';
 COMMENT ON COLUMN tidsserie.formaal IS 'Beskrivelse af formålet med tidsserien.';
-COMMENT ON COLUMN tidsserie.referenceramme IS 'Angivelse af tidsseriens referenceramme, eksempelvis IGS14_REPRO1';
 COMMENT ON COLUMN tidsserie.sridid IS 'Angivelse af transformerbart (så vidt muligt) koordinatsystem for tidsserien. Der findes ikke transformationer til/fra IGS14_REPRO1, det gør der der imod til ITFR2014 hvorfor dette angives som SRID. Transformationsmæssigt er IGS14 og ITRF2014 ækvivalente, så ved at angive ITRF2014 som SRID sikrer vi muligheden for at kunne transformere tidsserien til et andet system.';
 COMMENT ON COLUMN tidsserie.tstype IS 'Angivelse af tidsseriens type. Følgende værdier accepteres, 1: GNSS, 2: Nivellement';
 
