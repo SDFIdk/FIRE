8aef8d589ce72d0c61d5a359a00ad2c9fa48a3b4
Stefan Krebs <stkre@sdfi.dk>
2024-10-02 19:39:36 +0200

    ts/hts.py: Tilføj ts hts til udtræk af højdetidsserier

--- fire/cli/ts/hts.py
+++ fire/cli/ts/hts.py
@@ -33,3 +33,67 @@ HTS_PARAMETRE = {
     "sz": "sz",
 }
 
+
+@ts.command()
+@click.argument("objekt", required=True, type=str)
+@click.option(
+    "--parametre",
+    "-p",
+    required=False,
+    type=str,
+    default="t,decimalår,kote,sz",
+    help="""Vælg hvilke parametre i tidsserien der skal udtrækkes. Som standard
+sat til 't,decimalår,kote,sz'. Bruges værdien 'alle' udtrækkes alle mulige parametre
+i tidsserien.  Se ``fire ts hts --help`` for yderligere detaljer.""",
+)
+@click.option(
+    "--fil",
+    "-f",
+    required=False,
+    type=click.Path(writable=True),
+    help="Skriv den udtrukne tidsserie til Excel fil.",
+)
+@fire.cli.default_options()
+def hts(objekt: str, parametre: str, fil: click.Path, **kwargs) -> None:
+    """
+    Udtræk en Højdetidsserie.
+
+
+    "OBJEKT" sættes til enten et punkt eller et specifik navngiven tidsserie.
+    Hvis "OBJEKT" er et punkt udskrives en oversigt over de tilgængelige
+    tidsserier til dette punkt. Hvis "OBJEKT" er en tidsserie udskrives
+    tidsserien på skærmen. Hvilke parametre der udskrives kan specificeres
+    i en kommasepareret liste med ``--parametre``. Følgende parametre kan vælges::
+
+    \b
+        t               Tidspunkt for koordinatobservation
+        decimalår       Tidspunkt for koordinatobservation i decimalår
+        kote            Koordinatens z-komponent
+        sz              z-komponentens (kotens) spredning (i mm)
+
+    Tidsserien kan skrives til en fil ved brug af ``--fil``, der resulterer i
+    en csv-fil på den angivne placering. Denne fil kan efterfølgende åbnes
+    i Excel, eller et andet passende program, til videre analyse.
+
+    \b
+    **EKSEMPLER**
+
+    Vis alle tidsserier for punktet RDIO::
+
+        fire ts hts RDIO
+
+    Vis tidsserien "K-63-00909_HTS_81066" med standardparametre::
+
+        fire ts hts K-63-00909_HTS_81066
+
+    Vis tidsserie med brugerdefinerede parametre::
+
+        fire ts hts K-63-00909_HTS_81066 --parametre decimalår,kote,sz
+
+    Gem tidsserie med samtlige tilgængelige parametre::
+
+        fire ts hts K-63-00909_HTS_81066 -p alle -f RDIO_HTS_81066.xlsx
+    """
+    _udtræk_tidsserie(objekt, HøjdeTidsserie, HTS_PARAMETRE, parametre, fil)
+
+    return
