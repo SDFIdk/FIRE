dfb262b98dd5956baa35a10a663d96dd926cbd0b
Stefan Krebs <stkre@sdfi.dk>
2025-08-08 14:47:19 +0200

    Ret test af netanalyse

--- test/api/niv/test_netanalyse.py
+++ test/api/niv/test_netanalyse.py
@@ -1,8 +1,12 @@
-import pandas as pd
+from fire.api.niv.regnemotor import (
+    InternKote,
+    InternNivObservation,
+    RegneMotor,
+    GamaRegn,
+)
 
 from fire.cli.niv._netoversigt import (
-    analyser_subnet,
-    netgraf,
+    byg_netgeometri_og_singulære,
 )
 
 
@@ -14,7 +18,7 @@ def test_analyser_subnet():
     for N_subnet in range(1, 5):
         disjunkt_net.update({node: list(set(subnet) - {node}) for node in subnet})
 
-        liste_af_subnet = analyser_subnet(disjunkt_net)
+        liste_af_subnet = RegneMotor.find_subnet(disjunkt_net)
 
         assert len(liste_af_subnet) == N_subnet
 
@@ -26,7 +30,7 @@ def test_analyser_subnet():
             set(forbundet_net[0] + [subn[0] for subn in liste_af_subnet])
         )
 
-        liste_af_subnet = analyser_subnet(forbundet_net)
+        liste_af_subnet = RegneMotor.find_subnet(forbundet_net)
         assert len(liste_af_subnet) == 1
 
         subnet = [i + 100 for i in subnet]
@@ -46,53 +50,87 @@ def test_netgraf():
         "13",
     )
 
+    # Opbyg observationer som vi kan starte en motor ud fra.
     # Observationer opbygges som en symmetrisk, rettet, cirkulær graf, for at
     # efterligne en lukket nivellementspolygon.
-    obs = [
-        {"Fra": str(i % N), "Til": str(j % N)} for i in range(N) for j in (i + 1, i - 1)
+    observationer = [
+        InternNivObservation(
+            str(i % N), # fra
+            str(j % N), # til
+            *(6*[None]),
+        )
+        for i in range(N) for j in (i + 1, i - 1)
     ]
-    observationer = pd.DataFrame.from_records(obs)
-    (net, singulære) = netgraf(observationer, alle_punkter, fastholdte)
 
+    gamle_koter = [
+        InternKote(
+            p, None,None,None,
+            True if p in fastholdte else False
+        )
+        for p in alle_punkter
+    ]
+
+    motor = GamaRegn(observationer=observationer, gamle_koter=gamle_koter)
+
+    # Analyser net
+    net, ensomme_subnet, estimerbare_punkter = motor.netanalyse()
+    # Byg dataframes
+    net, singulære = byg_netgeometri_og_singulære(net, ensomme_subnet).values()
     # Check ingen singulære
     assert len(net) == N
     assert len(singulære) == 0
     assert len(net.keys()) == 3
 
     # Tilføj disjunkte net (singulære punkter)
-    obs.append({"Fra": "RMV", "Til": "SKG"})
-    obs.append({"Fra": "SKG", "Til": "RMV"})
-    obs.append({"Fra": "Hjemme", "Til": "Ude"})
-    obs.append({"Fra": "Ude", "Til": "Hjemme"})
+    observationer.append(InternNivObservation("RMV", "SKG", *(6*[None])))
+    observationer.append(InternNivObservation("SKG", "RMV", *(6*[None])))
+    observationer.append(InternNivObservation("Hjemme", "Ude", *(6*[None])))
+    observationer.append(InternNivObservation("Ude", "Hjemme", *(6*[None])))
 
     # ... og tilføj dem til alle_punkter
-    alle_punkter += ("RMV", "SKG", "Hjemme", "Ude")
+    gamle_koter.extend(
+        [
+            InternKote(p, *(3*[None])) for p in ("RMV", "SKG", "Hjemme", "Ude")
+        ]
+    )
+    motor = GamaRegn(observationer=observationer, gamle_koter=gamle_koter)
 
-    observationer = pd.DataFrame.from_records(obs)
-    (net, singulære) = netgraf(observationer, alle_punkter, fastholdte)
+    net, ensomme_subnet, estimerbare_punkter = motor.netanalyse()
+    net, singulære = byg_netgeometri_og_singulære(net, ensomme_subnet).values()
 
     assert len(net) == N
     assert len(singulære) == 4
     assert len(net.keys()) == 3
 
     # Fasthold nyt punkt
-    fastholdte += ("RMV",)
-    (net, singulære) = netgraf(observationer, alle_punkter, fastholdte)
+    for gk in gamle_koter:
+        if gk.punkt=='RMV':
+            gk.fasthold = True
+            break
+    motor = GamaRegn(observationer=observationer, gamle_koter=gamle_koter)
+
+    net, ensomme_subnet, estimerbare_punkter = motor.netanalyse()
+    net, singulære = byg_netgeometri_og_singulære(net, ensomme_subnet).values()
 
     assert len(net) == N + 2
     assert len(singulære) == 2
     assert len(net.keys()) == 3
 
     # Fra-Til har ikke en tilsvarende modsatrettet observation
-    obs.append({"Fra": "SKG", "Til": "Nord"})
-    obs.append({"Fra": "SKG", "Til": "Syd"})
-    obs.append({"Fra": "SKG", "Til": "Øst"})
-    obs.append({"Fra": "SKG", "Til": "Vest"})
+    observationer.append(InternNivObservation("SKG", "Nord", *(6*[None])))
+    observationer.append(InternNivObservation("SKG", "Syd", *(6*[None])))
+    observationer.append(InternNivObservation("SKG", "Øst", *(6*[None])))
+    observationer.append(InternNivObservation("SKG", "Vest", *(6*[None])))
 
-    alle_punkter += ("Nord", "Syd", "Øst", "Vest")
-
-    observationer = pd.DataFrame.from_records(obs)
-    (net, singulære) = netgraf(observationer, alle_punkter, fastholdte)
+    # ... og tilføj dem til alle_punkter
+    gamle_koter.extend(
+        [
+            InternKote(p, *(3*[None])) for p in ("Nord", "Syd", "Øst", "Vest")
+        ]
+    )
+    motor = GamaRegn(observationer=observationer, gamle_koter=gamle_koter)
+    net, ensomme_subnet, estimerbare_punkter = motor.netanalyse()
+    net, singulære = byg_netgeometri_og_singulære(net, ensomme_subnet).values()
 
     assert len(net) == N + 6
     assert len(singulære) == 2
