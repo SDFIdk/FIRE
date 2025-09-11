981d758ccffce8c64e20c903021345da64583e78
Stefan Krebs <stkre@sdfi.dk>
2025-09-11 14:55:45 +0200

    Tilføj test af lukkesum og rettelser til test_netanalyse

--- test/api/niv/test_netanalyse.py
+++ test/api/niv/test_netanalyse.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from fire.api.niv.regnemotor import (
     InternKote,
     InternNivObservation,
@@ -55,18 +57,16 @@ def test_netgraf():
     # efterligne en lukket nivellementspolygon.
     observationer = [
         InternNivObservation(
-            str(i % N), # fra
-            str(j % N), # til
-            *(6*[None]),
+            str(i % N),  # fra
+            str(j % N),  # til
+            *(6 * [None]),
         )
-        for i in range(N) for j in (i + 1, i - 1)
+        for i in range(N)
+        for j in (i + 1, i - 1)
     ]
 
     gamle_koter = [
-        InternKote(
-            p, None,None,None,
-            True if p in fastholdte else False
-        )
+        InternKote(p, None, None, None, True if p in fastholdte else False)
         for p in alle_punkter
     ]
 
@@ -82,16 +82,14 @@ def test_netgraf():
     assert len(net.keys()) == 3
 
     # Tilføj disjunkte net (singulære punkter)
-    observationer.append(InternNivObservation("RMV", "SKG", *(6*[None])))
-    observationer.append(InternNivObservation("SKG", "RMV", *(6*[None])))
-    observationer.append(InternNivObservation("Hjemme", "Ude", *(6*[None])))
-    observationer.append(InternNivObservation("Ude", "Hjemme", *(6*[None])))
+    observationer.append(InternNivObservation("RMV", "SKG", *(6 * [None])))
+    observationer.append(InternNivObservation("SKG", "RMV", *(6 * [None])))
+    observationer.append(InternNivObservation("Hjemme", "Ude", *(6 * [None])))
+    observationer.append(InternNivObservation("Ude", "Hjemme", *(6 * [None])))
 
     # ... og tilføj dem til alle_punkter
     gamle_koter.extend(
-        [
-            InternKote(p, *(3*[None])) for p in ("RMV", "SKG", "Hjemme", "Ude")
-        ]
+        [InternKote(p, *(3 * [None])) for p in ("RMV", "SKG", "Hjemme", "Ude")]
     )
     motor = GamaRegn(observationer=observationer, gamle_koter=gamle_koter)
 
@@ -104,7 +102,7 @@ def test_netgraf():
 
     # Fasthold nyt punkt
     for gk in gamle_koter:
-        if gk.punkt=='RMV':
+        if gk.punkt == "RMV":
             gk.fasthold = True
             break
     motor = GamaRegn(observationer=observationer, gamle_koter=gamle_koter)
@@ -117,16 +115,14 @@ def test_netgraf():
     assert len(net.keys()) == 3
 
     # Fra-Til har ikke en tilsvarende modsatrettet observation
-    observationer.append(InternNivObservation("SKG", "Nord", *(6*[None])))
-    observationer.append(InternNivObservation("SKG", "Syd", *(6*[None])))
-    observationer.append(InternNivObservation("SKG", "Øst", *(6*[None])))
-    observationer.append(InternNivObservation("SKG", "Vest", *(6*[None])))
+    observationer.append(InternNivObservation("SKG", "Nord", *(6 * [None])))
+    observationer.append(InternNivObservation("SKG", "Syd", *(6 * [None])))
+    observationer.append(InternNivObservation("SKG", "Øst", *(6 * [None])))
+    observationer.append(InternNivObservation("SKG", "Vest", *(6 * [None])))
 
     # ... og tilføj dem til alle_punkter
     gamle_koter.extend(
-        [
-            InternKote(p, *(3*[None])) for p in ("Nord", "Syd", "Øst", "Vest")
-        ]
+        [InternKote(p, *(3 * [None])) for p in ("Nord", "Syd", "Øst", "Vest")]
     )
     motor = GamaRegn(observationer=observationer, gamle_koter=gamle_koter)
     net, ensomme_subnet, estimerbare_punkter = motor.netanalyse()
@@ -135,3 +131,83 @@ def test_netgraf():
     assert len(net) == N + 6
     assert len(singulære) == 2
     assert len(net.keys()) == 6
+
+
+def test_lukkesum():
+    """
+    Tester et nivellement-netværk som dette:
+    A ------ B ------ E ------ F
+    |        |
+    |        |
+    D ------ C
+    Fra     Til     delta H
+    A       B        1.1
+    B       A       -1.
+    B       C        2.1
+    C       B       -2.
+    C       D        3.1
+    D       C       -3.
+    D       A       -6
+    A       D        5.7
+    B       E        2.5
+    E       B       -2.4
+    E       F        1.3
+    F       E       -1.2
+    """
+    obs = [
+        ["A", "B", 1.1],
+        ["B", "A", -1.0],
+        ["B", "C", 2.1],
+        ["C", "B", -2.0],
+        ["C", "D", 3.1],
+        ["D", "C", -3.0],
+        ["D", "A", -6],
+        ["A", "D", 5.7],
+        ["B", "E", 2.5],
+        ["E", "B", -2.4],
+        ["E", "F", 1.3],
+        ["F", "E", -1.2],
+    ]
+    # Opret observationer
+    observationer = [
+        InternNivObservation(
+            o[0],  # fra
+            o[1],  # til
+            None,
+            None,
+            1,  # afstand
+            o[2],  # delta H
+            None,
+            None,
+        )
+        for o in obs
+    ]
+
+    motor = GamaRegn(observationer=observationer, gamle_koter=[])
+
+    kredse = motor.lukkesum()
+    # tjek at der findes 1 kreds
+    assert len(kredse) == 1
+
+    # Test en lukket polygon
+    (summa_rho, lukkesum, H_frem, H_tilb, d_frem, d_tilb, deltaH, rho, afstande) = motor.lukkesum_af_polygon(["A", "B", "C", "D"]).values()
+    assert np.isclose(summa_rho, 0)
+    assert np.isclose(lukkesum, 0.3)
+    assert np.isclose(H_frem, 0.3)
+    assert np.isclose(H_tilb, -0.3)
+    assert np.allclose(rho, [0.1, 0.1, 0.1, -0.3])
+    assert np.allclose(deltaH, [1.05, 2.05, 3.05, -5.85])
+
+    # Test en blind linje ("åben" polygon)
+    (summa_rho, lukkesum, H_frem, H_tilb, d_frem, d_tilb, deltaH, rho, afstande) = motor.lukkesum_af_polygon(["B", "E", "F"], lukket=False).values()
+    assert np.isclose(summa_rho, 0.2)
+    assert np.isclose(lukkesum, 3.7)
+    assert np.isclose(H_frem, 3.8)
+    assert np.isclose(H_tilb, -3.6)
+    assert np.allclose(rho, [0.1, 0.1])
+    assert np.allclose(deltaH, [2.45, 1.25])
+
+    # Test en lukket polygon som ikke findes
+    (summa_rho, lukkesum, H_frem, H_tilb, d_frem, d_tilb, deltaH, rho, afstande) = motor.lukkesum_af_polygon(["B", "C", "E"], lukket=True).values()
+    assert np.isnan(summa_rho)
+    assert np.allclose(deltaH, [2.05, np.nan, -2.45], equal_nan = True)
