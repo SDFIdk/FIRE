08308a1b0de471e7db159b22490d4396e0c5522b
Stefan Krebs <stkre@sdfi.dk>
2025-10-31 16:40:36 +0100

    Tilføj lukkesumsberegning til `fire niv netoversigt`
    Den sjældent brugte `netoversigt`-kommando er blevet udvidet med funktionalitet
    til at beregne lukkesummer.
    
    Som standard bliver der nu foretaget en netanalyse som finder lukkede polygoner
    i nivellementnettet, og beregner lukkesummer for disse. Der findes nogle få
    input-parametre til identifikation af lukkede polygoner, men disse kan for nu
    ikke indstilles, da det vurderes, at standardopsætningen er tilstrækkelig til
    almindelig brug.
    
    Resultaterne gemmes som geojson-polygoner med de beregnede størrelser som
    attributter der kan inspiceres i et GIS-program. Derudover gemmes også de
    analyserede linjer som geojson.

--- src/fire/cli/niv/_netoversigt.py
+++ src/fire/cli/niv/_netoversigt.py
@@ -7,7 +7,14 @@ from fire.api.niv.regnemotor import (
     RegneMotor,
     GamaRegn,
 )
-
+from fire.api.niv.lukkesum import (
+    LinjeStats,
+    aggreger_multidigraf,
+)
+from fire.io.geojson import (
+    skriv_polygoner_geojson,
+    skriv_netoversigt_linjer_geojson,
+)
 from fire.cli.niv import (
     find_faneblad,
     niv,
@@ -24,7 +31,69 @@ from fire.cli.niv import (
     type=str,
 )
 def netoversigt(projektnavn: str, **kwargs) -> None:
-    """Opbyg netoversigt"""
+    """
+    Opbyg netoversigt og beregn lukkesummer
+
+    Læser fanebladet Observationer og udfører netanalyse der undersøger om observationerne
+    er sammenhængede og identificerer singulære punkter. Netanalysen gemmes i
+    sagsregnearket i fanebladene "Netgeometri" og "Singulære".
+
+    For hver nivellementslinje beregnes diskrepansen ϱ (rho), imellem frem- og
+    tilbagenivellement. Derudover identificeres lukkede polygoner i nettet, for hvilke der
+    beregnes lukkesummer. En polygon er kun gyldig hvis der findes både frem- og
+    tilbage-nivellement imellem alle punkter i polygonen. For hver polygon beregnes
+    lukkesum, summa rho, og samlet sigtelængde. Lukkesum og summa rho normaliseres desuden
+    med kvadratrod af sigtelængden.
+    Se nedenfor for de fulde lister af beregnede størrelser.
+
+    For hver polygon beregnes følgende størrelser::
+
+    \b
+    Størrelse [Enhed]        Beskrivelse
+    =======================  ====================================================================
+    Σϱ        [mm]           "Summa rho", samlet diskrepans imellem frem- og tilbage nivellement
+    (Σϱ)'     [mm/sqrt(km)]  "Summa rho mærke", normaliseret samlet diskrepans
+    ε         [mm]           Lukkesum for alle observationer langs polygonen
+    ε_frem    [mm]           Lukkesum for frem-observationer
+    ε_tilbage [mm]           Lukkesum for tilbage-observationer
+    ε'        [mm/sqrt(km)]  Lukkesum normaliseret efter samlet sigtelængde.
+    ε_frem'   [mm/sqrt(km)]  Frem-lukkesum normaliseret efter samlet frem-sigtelængde
+    ε_tilbage'[mm/sqrt(km)]  Tilbage-lukkesum normaliseret efter samlet tilbage-sigtelængde
+    L         [m]            Gennemsnit af samlet frem- og tilbage-sigtelængder.
+    L_frem    [m]            Sum af frem-sigtelængder
+    L_tilbage [m]            Sum af tilbage-sigtelængder
+
+    Resultaterne af lukkesumsberegningen gemmes som geojson, der kan åbnes i
+    et GIS-program.
+
+    For hver nivellementslinje beregnes der foruden diskrepansen ϱ, også gennemsnit af
+    frem- hhv. tilbage-observationer. Dette gøres i tilfælde af, at der findes mere end én
+    observation i hver retning::
+
+    \b
+    Størrelse [Enhed]         Beskrivelse
+    ========================  ====================================================================
+    ΔH         [m]            Gennemsnitlig "frem"-ΔH, af både frem- og tilbage observationer
+    ΔH_frem    [m]            Gennemsnit af frem-observationer
+    ΔH_tilbage [m]            Gennemsnit af tilbage-observationer
+    ϱ          [mm]           "rho", diskrepans imellem frem- og tilbage nivellement
+    ϱ'         [mm/sqrt(km)]  "rho mærke", normaliseret diskrepans
+    l          [m]            Gennemsnit af samlet frem- og tilbage-sigtelængder.
+    l_frem     [m]            Gennemsnit af frem-sigtelængder
+    l_tilbage  [m]            Gennemsnit af tilbage-sigtelængder
+    n_frem                    Antal frem-observationer
+    n_tilbage                 Antal tilbage-observationer
+
+    Resultaterne gemmes i en geojson-fil, som kan videreanalyseres bagefter i et
+    GIS-program. Ud fra disse tal er det muligt, fx at beregne Σϱ for en vilkårlig gruppe
+    af linjer eller lukkesum for andre polygoner, som dette program ikke har identificeret.
+
+    \b
+    Filnavn                                Beskrivelse
+    =====================================  =========================================
+    SAG-netoversigt-polygoner.geojson      Lukkesummer for alle fundne polygoner
+    SAG-netoversigt-linjer.geojson         Statistik for de enkelte linjer
+    """
     er_projekt_okay(projektnavn)
     fire.cli.print("Så kører vi")
 
@@ -33,31 +102,68 @@ def netoversigt(projektnavn: str, **kwargs) -> None:
 
     # Fjern slukkede observationer
     observationer = observationer[observationer["Sluk"] != "x"]
+    observationer.reset_index(inplace=True, drop=True)
 
-    motor = GamaRegn.fra_dataframe(observationer, punktoversigt, projektnavn=projektnavn)
+    motor = GamaRegn.fra_dataframe(
+        observationer, punktoversigt, projektnavn=projektnavn
+    )
 
     # Analyser net
     net_uden_ensomme, ensomme_subnet, estimerbare_punkter = motor.netanalyse()
     if ensomme_subnet:
-        fire.cli.print(f"ADVARSEL: Manglende fastholdt punkt i mindst et subnet! Forslag til fastholdte punkter i hvert subnet:", bg="yellow", fg="black")
+        fire.cli.print(
+            f"ADVARSEL: Manglende fastholdt punkt i mindst et subnet! Forslag til fastholdte punkter i hvert subnet:",
+            bg="yellow",
+            fg="black",
+        )
         for i, forslag in enumerate(ensomme_subnet):
             fire.cli.print(f"  Subnet {i}: {forslag}", fg="red")
 
     resultater = byg_netgeometri_og_singulære(net_uden_ensomme, ensomme_subnet)
 
+    # Lukkesumsberegning
+    polygoner = motor.beregn_lukkesummer()
+    if len(polygoner) != 0:
+        filnavn = f"{projektnavn}-netoversigt-polygoner.geojson"
+        fire.cli.print(f"Skriver '{filnavn}'")
+        skriv_polygoner_geojson(filnavn, motor._gamle_koter, polygoner)
+
+    digraf = aggreger_multidigraf(motor.multidigraf)
+
+    # Hver linje ligger teknisk set dobbelt, hvor frem- og tilbage linjerne har omvendt fortegn.
+    # Så vi looper over den simple version af grafen for kun at få hver linje med én gang
+    # og hiver den pågældende linjestatistik ud.
+    graf = digraf.to_undirected(reciprocal=True)
+    linjer: list[LinjeStats] = [
+        digraf[fra][til]["linjestats"] for fra, til in graf.edges
+    ]
+    linjer = [l.omregn_til_mm() for l in linjer]
+
+    filnavn = f"{projektnavn}-netoversigt-linjer.geojson"
+    fire.cli.print(f"Skriver '{filnavn}'")
+    skriv_netoversigt_linjer_geojson(filnavn, motor._gamle_koter, linjer)
+
     skriv_ark(projektnavn, resultater, "-netoversigt")
 
     singulære_punkter = tuple(sorted(resultater["Singulære"]["Punkt"]))
     fire.cli.print(
-        f"Fandt {len(singulære_punkter)} singulære punkter: {singulære_punkter}"
+        f"Fandt {len(singulære_punkter)} singulære punkter."
     )
+    fire.cli.print(f"Fandt {len(polygoner)} polygoner.")
 
 
-def byg_netgeometri_og_singulære(net_uden_ensomme: dict[str,list], ensomme_subnet: list) -> dict[pd.DataFrame]:
+def byg_netgeometri_og_singulære(
+    net_uden_ensomme: dict[str, list], ensomme_subnet: list
+) -> dict[pd.DataFrame]:
     """Omsætter en netværksgraf og dens ensomme punkter til dataframes"""
     # Nu kommer der noget grimt...
     # Tving alle rækker til at være lige lange, så vi kan lave en dataframe af dem
-    max_antal_naboer = max([len(net_uden_ensomme[e]) for e in net_uden_ensomme])
+
+    # Undgå ValueError pga max([]) hvis ingen fastholdte punkter.
+    max_antal_naboer = 0
+    if net_uden_ensomme:
+        max_antal_naboer = max([len(net_uden_ensomme[e]) for e in net_uden_ensomme])
+
     nyt = {}
     for punkt in net_uden_ensomme:
         naboer = list(sorted(net_uden_ensomme[punkt])) + max_antal_naboer * [""]
